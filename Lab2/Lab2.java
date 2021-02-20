08346f7bbaa4f10fbe70757b0057d39c0b90ba31
Tieguy01 <tieguy10501@gmail.com>
2021-02-20 17:28:55 -0500

    Implements file reading

--- Lab2/Lab2.java
+++ Lab2/Lab2.java
@@ -1,7 +1,16 @@
+/**
+ * @author Ethan Shahan
+ */
+
 package Lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Lab2 {
 
+    // print array of Objects
     public static void printArrayNonGen(Object[] arr) {
         System.out.print("{");
         for (int i = 0; i < arr.length - 1; i++) {
@@ -11,6 +20,7 @@ public class Lab2 {
         System.out.println();
     }
 
+    // prints array of Integers
     public static void printArray(Integer[] arr) {
         System.out.print("{");
         for (int i = 0; i < arr.length - 1; i++) {
@@ -20,6 +30,7 @@ public class Lab2 {
         System.out.println();
     }
 
+    // prints array of Doubles
     public static void printArray(Double[] arr) {
         System.out.print("{");
         for (int i = 0; i < arr.length - 1; i++) {
@@ -29,6 +40,7 @@ public class Lab2 {
         System.out.println();
     }
 
+    // prints array of Characters
     public static void printArray(Character[] arr) {
         System.out.print("{");
         for (int i = 0; i < arr.length - 1; i++) {
@@ -38,6 +50,7 @@ public class Lab2 {
         System.out.println();
     }
 
+    // prints array of Strings
     public static void printArray(String[] arr) {
         System.out.print("{");
         for (int i = 0; i < arr.length - 1; i++) {
@@ -47,6 +60,7 @@ public class Lab2 {
         System.out.println();
     }
 
+    // prints array of generic type T
     public static <T> void printArrayGen(T[] arr) {
         System.out.print("{");
         for (int i = 0; i < arr.length - 1; i++) {
@@ -56,6 +70,7 @@ public class Lab2 {
         System.out.println();
     }
 
+    //  returns the max Comparable value in an array (non-generic)
     public static Comparable getMax(Comparable[] arr) {
         Comparable max = arr[0];
         for (int i = 1; i < arr.length; i++) {
@@ -66,6 +81,7 @@ public class Lab2 {
         return max;
     }
 
+    // returns the max value of generic type T in an array
     public static <T extends Comparable<T>> T getMaxGen(T[] arr) {
         T max = arr[0];
         for (int i = 1; i < arr.length; i++) {
@@ -77,32 +93,83 @@ public class Lab2 {
     }
 
     public static void main(String[] args) {
-        Integer[] intArray = {1, 2, 3, 4, 5};
-        Double[] doubArray = {1.1, 2.2, 3.3, 4.4};
-        Character[] charArray = {'H', 'E', 'L', 'L', 'O'};
-        String[] strArray = {"once", "upon", "a", "time"};
+        Scanner scan1 = new Scanner(Lab2.class.getResourceAsStream("00.in")); // scanner getting each line from a file
+        
+        String tempLine = scan1.nextLine(); // string to store line from scan1
+        Scanner scan2 = new Scanner(tempLine); // scanner to scan trough a line from scan1
 
+        // makes array of integers from the first line of the file
+        List<Integer> intList = new ArrayList<>();
+        while(scan2.hasNextInt()) {
+            intList.add(scan2.nextInt());
+        }
+        Integer[] intArray = intList.toArray(new Integer[0]);
+
+        // reset the second scan2 to the next line in scan1
+        scan2.close();
+        tempLine = scan1.nextLine();
+        scan2 = new Scanner(tempLine);
+
+        // makes array of doubles from the second line of the file
+        List<Double> doubList = new ArrayList<>();
+        while(scan2.hasNextDouble()) {
+            doubList.add(scan2.nextDouble());
+        }
+        Double[] doubArray = doubList.toArray(new Double[0]);
+
+        // reset the second scan2 to the next line in scan1
+        scan2.close();
+        tempLine = scan1.nextLine();
+        scan2 = new Scanner(tempLine);
+
+        // makes array of characters from the third line of the file
+        List<Character> charList = new ArrayList<>();
+        while(scan2.hasNext()) {
+            charList.add(scan2.next().charAt(0));
+        }
+        Character[] charArray = charList.toArray(new Character[0]);
+
+        // reset the second scan2 to the next line in scan1
+        scan2.close();
+        tempLine = scan1.nextLine();
+        scan2 = new Scanner(tempLine);
+
+        // makes array of strings from the fourth line of the file
+        List<String> strList = new ArrayList<>();
+        while(scan2.hasNext()) {
+            strList.add(scan2.next());
+        }
+        String[] strArray = strList.toArray(new String[0]);
+
+        // close both the scanners
+        scan2.close();
+        scan1.close();
+
+        // print arrays using non generic method
         printArrayNonGen(intArray);
         printArrayNonGen(doubArray);
         printArrayNonGen(charArray);
         printArrayNonGen(strArray);
 
+        // print arrays using overloaded methods
         printArray(intArray);
         printArray(doubArray);
         printArray(charArray);
         printArray(strArray);
 
+        // print arrays using generic method
         printArrayGen(intArray);
         printArrayGen(doubArray);
         printArrayGen(charArray);
         printArrayGen(strArray);
 
+        // print max value of each array using non generic method
         System.out.println(getMax(intArray));
         System.out.println(getMax(doubArray));
         System.out.println(getMax(charArray));
         System.out.println(getMax(strArray));
-        
 
+        // print max value of each array using generic method
         System.out.println(getMaxGen(intArray));
         System.out.println(getMaxGen(doubArray));
         System.out.println(getMaxGen(charArray));
--- Lab2/README.md
+++ Lab2/README.md
@@ -1,3 +1,12 @@
-Ethan Shahan
+Ethan Shahan Lab #2 Java Generics
+Lab: 17 Tues/Thursday 4:50-6:05
 
-Email: eshahan@u.rochester.edu
+Contact Info:
+    Email: eshahan@u.rochester.edu
+
+Lab Explanation:
+    In this lab, contained within the class Lab2, there are six methods for printing arrays of different types: the method printArrayNonGen takes an array of Objects as its parameter, the printArray methods are four overloaded methods for printing arrays of type int, double, char, and String, and the method printArrayGen uses the parameter of an array of generic type T to support all four of these array types. There are also two methods for getting the max value from an array of any type: getMax takes an array of type Comparable, while the type safe version getMaxGen takes an array of generic type T (as long as it extends Comparable).
+
+    All the arrays used in this class are taken from the text file 00.in. In this text file the first line is turned into an array of integers, the second line is turned into an array of doubles, the third line is turned into an array of characters, and the fourth line is turned into an array of Strings. All values are separated by spaces in the the text file.
+
+    All results are printed to the console.

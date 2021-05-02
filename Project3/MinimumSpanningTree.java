ec24e31ce3a767fc6ffd5bed455f32ccbd40470c
Tieguy01 <tieguy10501@gmail.com>
2021-05-02 00:33:10 -0400

    MST fixed

--- Project3/MinimumSpanningTree.java
+++ Project3/MinimumSpanningTree.java
@@ -5,14 +5,17 @@ public class MinimumSpanningTree {
     private class SetNode {
 
         SetNode set;
-        private Node node;
+        private Bag<Node> nodes;
+        int length;
 
-        public void setNode(Node node) {
-            this.node = node;
+        public SetNode() {
+            nodes = new Bag<>();
+            length = 1;
         }
 
-        public Node getNode() {
-            return node;
+        public void addNode(Node n) {
+            nodes.add(n);
+            length++;
         }
     }
 
@@ -34,14 +37,41 @@ public class MinimumSpanningTree {
             vertexSet[i].set = vertexSet[i];
         }
 
-        while (!edgePQ.isEmpty() && mst.size() < map.getNumIntersections() - 1) {
+        int vertexSetLength = 1;
+
+        while (!edgePQ.isEmpty() && vertexSetLength < map.getNumIntersections()) {
             Edge e = edgePQ.delMin();
             Node v = e.getEitherIntersection();
             Node w = e.getOtherIntersection(v);
+
             SetNode vSet = vertexSet[v.getIndex()].set;
             SetNode wSet = vertexSet[w.getIndex()].set;
+
+            if (vSet.nodes.size() == 0) vSet.addNode(v);
+            if (wSet.nodes.size() == 0) wSet.addNode(w);
+
             if (!vSet.equals(wSet)) {
-                vertexSet[w.getIndex()].set = vSet;
+                SetNode parentSet;
+                SetNode lesserSet;
+
+                if (vSet.length > wSet.length) {
+                    parentSet = vSet;
+                    lesserSet = wSet;
+                } else {
+                    parentSet = wSet;
+                    lesserSet = vSet;
+                }
+
+                for (Node n : lesserSet.set.nodes) {
+                    // System.out.println("n: " + n.getID());
+                    vertexSet[n.getIndex()].set = parentSet;
+                    parentSet.addNode(n);
+                }
+                lesserSet = null;
+
+                vertexSetLength = parentSet.length;
+                // System.out.println("parentSet: " + vertexSetLength);
+
                 mst.add(e);
             }
         }
